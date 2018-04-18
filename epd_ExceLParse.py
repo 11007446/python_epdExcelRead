5cee8276d9b41914ee19d810afd8713d1c47d733
11007446 <11007446@qq.com>
2018-04-18 18:02:15 +0800

    20180418

---
+++ epd_ExceLParse.py
@@ -0,0 +1,35 @@
+import openpyxl
+from epd_util  import Epd_util
+
+# epd_batchinfo
+# epd_batchstatement
+# epd_pginfo
+# epd_expertinfo
+# epd_projectinfo
+# epd_log    
+
+
+def loadExcelData(filepath):
+    wb = openpyxl.load_workbook(filepath)  
+    for sheet in wb:
+        parseSheetData(sheet)
+    pass
+
+
+
+def parseSheetData(sheet):
+    epd_u =  Epd_util()
+    sheet_title = sheet.title
+    sqlpart =  epd_u.BASE_SQL[sheet_title]
+    pass
+
+# wb = openpyxl.load_workbook('example.xlsx')  
+# sheet = wb.get_sheet_by_name('Sheet1')  
+# for row in sheet.iter_rows():  
+#     for cell in row:  
+#         print(cell.coordinate, cell.value)  
+# print('--- END OF ROW ---')  
+    
+
+
+
---
+++ epd_util.py
@@ -0,0 +1,13 @@
+class Epd_util(object):
+    # d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+    BASE_SQL = {
+        'epd_batchinfo':'INSERT INTO epd_batchinfo (batchid,batchno,batchnum,batchyear,batchym,batchgroupid,pgcount,pcount,expertcount,expertnum,batchtype,batchstate,importdate,intervalstart,intervalend) values ',
+        'epd_batchstatement':'insert into epd_batchstatement (bsid,batchgroupid,expertid,expertname,expertsex,expertidnumber,expertaddress,expertpostnumber,expertmailbox,appraisepronum,payment,tax,remittancecharge,comment,batchtype,expertidtype,expertbirthdate,expertbanktype,expertbankaddress,expertbankbranch,expertbanknumber,bankUser,expertmobile) values ',
+        'epd_pginfo':'insert into epd_pginfo (pgid,pgname,pgyear,pgmanager,pgcudatee,pgreviewcompletedate,pgcatanodeid,projnum,pgexpertnum,pgexpertsubmitnum,zjbatchid,batchgroupid,pgmoney,importdate,initFlag,iszj) values ',
+        'epd_expertinfo':'insert into epd_expertinfo (expertid,expertname,expertinfoid,pgid,appraisesubmitdate,importdate) values ',
+        'epd_projectinfo':'insert into epd_projectinfo (pid,pgid,pname,catapayment,importdate) values ',
+        'epd_log':'insert into epd_log (logid,action,actiontime,account,accountname) values '
+    }
+
+    
+    pass
